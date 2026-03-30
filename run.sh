929da69cce8720c3b796f4ca8dfe3215c72711ee
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2026-03-30 13:27:25 +0000

    simple run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Build from workspace root
+# cd /ros2_ws
+colcon build
+
+# Source workspace
+source /opt/ros/foxy/setup.bash
+source ./install/local_setup.bash
+
+echo "Ensure the F1Tenth simulator is already running."
+echo "Launching evaluation..."
+ros2 launch state_machine state_machine.py

1adf876a6a298fe60ef3e42b70228edc02b5494a
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2026-04-25 09:31:45 +0000

    fix: launch entrypoint

--- src/autodrive_devkit/launch/bringup_headless.launch.py
+++ src/autodrive_devkit/launch/bringup_headless.launch.py
@@ -24,12 +24,24 @@
 
 ################################################################################
 
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('pure_pursuit')
+
+    config_arg = DeclareLaunchArgument(
+        name='config',
+        default_value=os.path.join(pkg_share, 'config', 'controller_manager.yaml'),
+        description='Full path to the controller_manager YAML config file'
+    )
 
     return LaunchDescription([
+        config_arg,
         Node(
             package='autodrive_roboracer',
             executable='autodrive_bridge',
@@ -37,4 +49,32 @@ def generate_launch_description():
             emulate_tty=True,
             output='screen',
         ),
+        Node(
+            package='state_machine',
+            executable='state_machine',
+            name='state_machine',
+            output='screen',
+        ),
+        # Node(
+        #     package='gap_finder',
+        #     executable='gap_finder_node',
+        #     name='gap_finder_node',
+        #     output='screen',
+        # ),
+        Node(
+            package='pure_pursuit',
+            executable='controller_manager_node',
+            name='controller_manager_node',
+            parameters=[
+                LaunchConfiguration('config')
+            ],
+            output='screen',
+        ),
+        Node(
+            package='perception',
+            executable='detect',
+            name='detect',
+            parameters=[{'use_sim_time': True}],
+            output='screen',
+        )
     ])
